8bc929bacec08ce2af8d74cb087af83b8c02e3e6
amitmishra2101-blip <amitmishra2101@gmail.com>
2026-04-07 09:08:48 +0530

    add voice-studio page

---
+++ app/voice studio/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import VoiceStudio from "../../component/voicestudio";
+
+export default function VoiceStudioPage() {
+  return (
+    <div className="min-h-screen bg-black">
+      <VoiceStudio />
+    </div>
+  );
+}
